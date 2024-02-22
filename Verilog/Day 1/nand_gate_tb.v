908ac82aacde5ac0a85c1cd77ba0f21e285c19bc
MAC-222 <130531986+MAC-222@users.noreply.github.com>
2024-02-22 14:02:30 +0530

    Update nand_gate_tb.v

--- Verilog/Day 1/nand_gate_tb.v
+++ Verilog/Day 1/nand_gate_tb.v
@@ -12,13 +12,13 @@ initial begin
 end
 //apply inputs
 initial begin
-  a=1'b0; b=1'b0;
+  a=0; b=0;
   #10
-  a=1'b0; b=1'b1;
+  a=0; b=1;
   #10
-  a=1'b1; b=1'b0;
+  a=1'b1; b=0;
   #10
-  a=1'b1; b=1'b1;
+  a=1; b=1;
   #10
 $finish();
 end
