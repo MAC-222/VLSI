c853d6032faf960358acc2afff7eadd91cc33802
MAC-222 <130531986+MAC-222@users.noreply.github.com>
2024-02-22 13:01:23 +0530

    Create nand_gate_tb.v

---
+++ Verilog/Day 1/nand_gate_tb.v
@@ -0,0 +1,25 @@
+// Code nand gate testbench 
+module nand_gate_tb;
+  reg a,b;
+  wire y;
+//instantiate and_gate module
+ nand_gate nandgate(.a(a),.b(b),.y(y));
+//dump and monitor informations
+initial begin 
+  $monitor("a=%0d b=%0d y=%0d",a,b,y);
+  $dumpfile("nand_gate.vcd");
+  $dumpvars(1,nand_gate_tb);
+end
+//apply inputs
+initial begin
+  a=1'b0; b=1'b0;
+  #10
+  a=1'b0; b=1'b1;
+  #10
+  a=1'b1; b=1'b0;
+  #10
+  a=1'b1; b=1'b1;
+  #10
+$finish();
+end
+endmodule
