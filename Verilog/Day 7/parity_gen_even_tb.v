5a0f32e8ea9841b4a05c1bd4bced534949a517ae
MAC-222 <130531986+MAC-222@users.noreply.github.com>
2024-02-29 14:37:07 +0530

    Update parity_gen_even_tb.v

--- Verilog/Day 7/parity_gen_even_tb.v
+++ Verilog/Day 7/parity_gen_even_tb.v
@@ -3,13 +3,21 @@ module parity_gen_even_tb;
   wire p;
   parity_gen_even pe(.a(a),.b(b),.c(c),.p(p));
   initial begin
-    a=0; b=1; c=1;
+    a=0; b=0; c=0;
+    #10
+    a=0; b=0; c=1;
+    #10
+    a=0; b=1; c=0;
     #10
     a=0; b=1; c=1;
     #10
     a=1; b=0; c=0;
     #10
-    a=0; b=0; c=1;
+    a=1; b=0; c=1;
+    #10
+    a=1; b=1; c=0;
+    #10
+    a=1; b=1; c=1;
     #10
     $finish;
   end
