bc844b3a46a8fa932167925b3c5b7992179a30cb
MAC-222 <130531986+MAC-222@users.noreply.github.com>
2024-03-01 22:02:48 +0530

    Delete Verilog/Day 8/odd_number.v

--- Verilog/Day 8/odd_number.v
+++
@@ -1,9 +0,0 @@
-module odd (
-  input reg [7:0]a);
-  always @(*)begin
-    if(a%2==1)
-      $display("It is an odd number");
-    else
-      $display("It is not an odd number");
-  end
-endmodule
