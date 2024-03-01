b7eeab8032e856bc87c4e6c2a5139bd38480e1fd
MAC-222 <130531986+MAC-222@users.noreply.github.com>
2024-03-01 22:01:25 +0530

    Update and rename even_number.v to evenorodd_number.v

--- Verilog/Day 8/evenorodd_number.v
+++ Verilog/Day 8/evenorodd_number.v
@@ -1,9 +1,9 @@
-module even (
+module evenorodd (
   input reg [7:0]a);
   always @(*)begin
     if(a%2==0)
       $display("It is an even number",a);
     else
-      $display("It is not an even number",a);
+      $display("It is an odd number",a);
   end
 endmodule
