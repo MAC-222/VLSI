b3d060e8e3fcabf2314d178526443bfbe64cd950
MAC-222 <130531986+MAC-222@users.noreply.github.com>
2024-03-01 22:15:35 +0530

    Update primechecker.v

--- Verilog/Day 7/primechecker.v
+++ Verilog/Day 7/primechecker.v
@@ -4,6 +4,6 @@ module PrimeChecker (
     if((a==2)||(a%2==1)&&(a%3==1)||(a%3==2))
       $display("It is a prime number",a);
     else
-      $display("It is not a prime number",a);
+      $display("It is a composite number",a);
   end
 endmodule
