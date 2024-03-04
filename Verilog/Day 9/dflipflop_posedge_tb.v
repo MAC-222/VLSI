6c4c81ff12ce6551f632897ba2baf663a40959d5
MAC-222 <130531986+MAC-222@users.noreply.github.com>
2024-03-04 17:05:50 +0530

    Create dflipflop_posedge_tb.v

---
+++ Verilog/Day 9/dflipflop_posedge_tb.v
@@ -0,0 +1,22 @@
+module dflipflop_posedge_tb;
+  reg clk,d;
+  wire q;
+  dflipflop dff(
+    .clk(clk),
+    .d(d),
+    .q(q)
+  );
+  initial begin
+    $monitor("t=%d clk=%d d=%d q=%d",$time,clk,d,q);
+    $dumpfile("dflipflop_posedge.vcd");
+    $dumpvars(1,dflipflop_posedge_tb);
+  end
+  initial begin
+    clk=0;
+    d = 0;
+    #200
+    $finish;
+    end
+always #5 clk = ~clk;
+always #10 d = ~d;
+endmodule
