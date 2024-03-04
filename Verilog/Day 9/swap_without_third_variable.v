0d98153d05a916de3c949e6e512397c3871c4c63
MAC-222 <130531986+MAC-222@users.noreply.github.com>
2024-03-04 14:55:18 +0530

    Update swap_without_third_variable.v

--- Verilog/Day 9/swap_without_third_variable.v
+++ Verilog/Day 9/swap_without_third_variable.v
@@ -1,11 +1,17 @@
+// Code your design here
 module swap_without_third_variable;
-     int x=11,y=89;
-     initial begin 
-    x = x + y; 
-    y = x - y; 
-    x = x - y;
-       $display(" Values of x=%0d y=%0d",x,y);
-     
-end
+    int x, y;
+    initial begin 
+        // Assign values to x and y (you can replace these with your own initialization logic)
+        x = $random %16;
+        y = $random %16;
+        // Display initial values
+        $display("Initial values: x=%0d, y=%0d", x, y);
+        // Swap the values without using a third variable
+        x = x + y; 
+        y = x - y; 
+        x = x - y;    
+        // Display swapped values
+        $display("Values after swap: x=%0d, y=%0d", x, y);
+    end
 endmodule
-  
