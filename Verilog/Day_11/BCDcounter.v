b05e40264027ef47a361944aa5cb8e71d42b2103
MAC-222 <130531986+MAC-222@users.noreply.github.com>
2024-03-05 22:33:06 +0530

    Create BCDcounter.v

---
+++ Verilog/Day_11/BCDcounter.v
@@ -0,0 +1,19 @@
+module BCDcounter(
+  input clk, reset,
+  output reg [7:0] out
+);
+  
+  always @(posedge clk or  posedge reset) begin
+    
+    if (reset)
+      out <= 4'h0;
+    
+    else if (out == 4'ha)
+      out <= 4'h0;
+    
+    else
+      out <= out + 1;
+    
+  end
+  
+endmodule
