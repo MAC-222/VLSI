bb1c7ff6ff64b8d4fe0918246042ba61381c2906
Macklin Eniya C <130531986+MAC-222@users.noreply.github.com>
2024-03-17 22:12:02 +0530

    Create Declaring_wires.v

---
+++ Assignment/Declaring_wires.v
@@ -0,0 +1,14 @@
+`default_nettype none
+module top_module(
+    input a,
+    input b,
+    input c,
+    input d,
+    output out,
+    output out_n   ); 
+    wire x,y;
+    and(x,a,b);
+    and(y,c,d);
+    or(out,x,y);
+    not(out_n,out);
+endmodule
