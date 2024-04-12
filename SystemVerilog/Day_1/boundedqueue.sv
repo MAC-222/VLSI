53441dd5df8576e3779e55213ecdcab32ba0176f
Macklin Eniya C <130531986+MAC-222@users.noreply.github.com>
2024-04-12 22:17:21 +0530

    Create boundedqueue.sv

---
+++ SystemVerilog/Day_1/boundedqueue.sv
@@ -0,0 +1,35 @@
+module boundedqueue;
+  
+  int m[$:10] = {90,29,22,34,56,78,11,63,35};
+  
+  
+  initial begin
+     m.insert(8,4);
+     $display(m);
+    
+    m.push_front(2);
+    $display(m);
+    
+    m.sort();
+    $display(m);
+    
+    m.delete(3);
+    $display(m);
+    
+    m.push_back(100);
+    $display(m);  
+    
+    m.insert(2,8);
+    $display(m);
+    
+    m.push_front(3);
+    $display(m);
+    
+    //     m.push_back(98);
+//     $display(m);
+    
+    m.rsort();
+    $display(m);
+    
+  end
+endmodule
