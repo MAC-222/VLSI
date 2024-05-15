95744aa056855715f07a0184458d599e27fdca4c
Macklin Eniya C <130531986+MAC-222@users.noreply.github.com>
2024-05-15 14:52:12 +0530

    Create associativearray.sv

---
+++ SystemVerilog/Day_5/associativearray.sv
@@ -0,0 +1,21 @@
+typedef enum {TRANS, RECEIVE, REPEATER} tr_type;
+class transaction;
+  bit [31:0] data;
+  int id;
+endclass
+
+module class_example;
+  transaction tr[tr_type];
+  initial begin
+    tr[TRANS]    = new();
+    tr[RECEIVE]  = new();
+    tr[REPEATER] = new();
+  
+    foreach(tr[i]) begin 
+      tr[i].data = i*i;
+      tr[i].id = i+1;
+    end
+    
+    foreach(tr[i]) $display("tr[%0s].data = %0d, tr[%0s].id = %0d",i.name, tr[i].data, i.name, tr[i].id);
+  end
+endmodule
