7141868ff2baaae8b1a6935d4879aa7483d72a6c
Macklin Eniya C <130531986+MAC-222@users.noreply.github.com>
2024-06-24 14:23:54 +0530

    Create nonblocking_event.sv

---
+++ SystemVerilog/Event/nonblocking_event.sv
@@ -0,0 +1,22 @@
+module exevent;
+  event e1;
+  task Process_A;
+    #10;
+    $display("before triggering of the event ",$time);
+    ->>e1;
+    $display("after triggering of the event ",$time);
+  endtask
+  
+  task Process_B;
+    $display("waiting for the event ",$time);
+    @e1;
+    $display("capture of the event ",$time);
+  endtask
+  
+  initial begin
+    fork 
+      Process_A;
+      Process_B;
+    join
+  end
+endmodule
