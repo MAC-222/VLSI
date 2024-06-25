3b5e3cedb4fdc8c9b0fd3a9242846b87b0747ca9
Macklin Eniya C <130531986+MAC-222@users.noreply.github.com>
2024-06-25 06:28:20 +0530

    Create logic_data_type.sv

---
+++ SystemVerilog/Datatype/logic_data_type.sv
@@ -0,0 +1,10 @@
+// Code your design here
+module logic_data_type;
+  logic [3:0] dt;
+  
+  initial begin
+    $display("\tdefault value of logic dt = %0b ",dt);
+    dt = 4'b1010;
+    $display("\tvalue of logic dt after initialization = %0b ",dt);
+  end
+endmodule
