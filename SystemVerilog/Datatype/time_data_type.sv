85dd2b3ca184ad501d5d3d3255fe2ce5fb3243df
Macklin Eniya C <130531986+MAC-222@users.noreply.github.com>
2024-06-25 06:42:01 +0530

    Create time_data_type.sv

---
+++ SystemVerilog/Datatype/time_data_type.sv
@@ -0,0 +1,11 @@
+// Code your design here
+module time_data_type;
+  time clock;
+  
+  initial begin
+    $display("\tinitial value of clock = %0d ",clock);
+    #10;
+    clock = $time;
+    $display("\tvalue of clock after initialization = %0d ",clock);
+  end
+endmodule
