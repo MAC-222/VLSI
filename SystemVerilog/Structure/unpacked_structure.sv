3c16f51a10b4c1cbd8d8f4953ac31b10e78d0131
Macklin Eniya C <130531986+MAC-222@users.noreply.github.com>
2024-06-28 12:48:47 +0530

    Create unpacked_structure.sv

---
+++ SystemVerilog/Structure/unpacked_structure.sv
@@ -0,0 +1,16 @@
+struct{
+    string name;
+    bit[15:0] salary;
+    byte id;
+ }employee_s; 
+module struct1;
+  initial begin 
+ 
+employee_s = '{ "sam",16897,123};
+  
+  $display("\n employee_s = %p" ,employee_s); 
+
+  $display("\n size of employee_s: %0d", $bits(employee_s));
+
+  end  
+endmodule
