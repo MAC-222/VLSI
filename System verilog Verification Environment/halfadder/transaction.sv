0a996e526fd7d33ae4a29e351f8156e0362aed50
Macklin Eniya C <130531986+MAC-222@users.noreply.github.com>
2024-08-07 00:10:38 +0530

    Update transaction.sv

--- System verilog Verification Environment/halfadder/transaction.sv
+++ System verilog Verification Environment/halfadder/transaction.sv
@@ -1,11 +1,12 @@
 class transaction;
-  rand bit a;
-  rand bit b;
+  bit a = 1;
+  bit b = 0;
   bit sum;
   bit carry;
   
   function void display(string name);
     $display("%s",name);
-    $display("a = %0d b = %0d sum = %0d carry = %0d",a,b,sum,carry);
+    $display("a = %0d b = %0d ",a,b);
+    $display("sum = %0d carry = %d",sum,carry);
   endfunction
 endclass  
